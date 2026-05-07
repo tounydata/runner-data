2f088d642fc8c21a02e6e351d9fd8f044999f9f6
tounydata <tony.bollecker@hotmail.fr>
2026-05-07 12:17:42 +0200

    Set Vite base path for GitHub Pages

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import { resolve } from 'path'
 
 export default defineConfig({
+  base: '/runner-data/',
   plugins: [react()],
   resolve: {
     alias: {
